8c935a18e4aa3c71f64dc66d92cbb9e402041c9d
kartikraj <47671868+kartikraj@users.noreply.github.com>
2020-04-11 13:05:34 +0530

    updated code for organizing containers (#30)
    * Create Hackerrank
    
    * Create sherlock and squares
    
    * Delete Hackerrank
    
    * Create name of fie
    
    * Delete name of fie
    
    * Update OrganisingContainers.cpp

--- Kartik/OrganisingContainers.cpp
+++ Kartik/OrganisingContainers.cpp
@@ -3,52 +3,70 @@
 using namespace std;
 
 // Complete the organizingContainers function below.
-string organizingContainers(vector<vector<int>> container)
-{
-    int i,j,a,b;
-    int vect[100][100];
-    int cont[100][100];
-    int r[20],c[20];
-    
-for (int i = 0; i < container.size(); i++)
-{ 
-        for (int j = 0; j < container[i].size(); j++)
-        { 
-             vect[i][j]=cont[i][j]; 
-        } 
-}
+string organizingContainers(vector<vector<int>> container) {
+int i,j,a,b,x;
+long long int r[100],c[100];
+
 
   for( i=0; i<container.size(); i++ )
   {
        r[i]=0;
-       for(j=0;j<container[i].size();j++)
-       r[i] = r[i] + vect[i][j];
+       for(j=0;j<container.size();j++)
+       {
+                r[i] = r[i] + container[i][j];
+                r[i]%=1000000000;
+                
+       }
+       cout<<i<<" "<<r[i]<<endl;
 }
   
     for( j=0; j<container.size(); j++ )
     {
         c[j]=0;
-      for(i=0;i<container[j].size();i++)       
-       c[j] = c[j] + vect[i][j];
-       }
+      for(i=0;i<container.size();i++)  
+      {     
+       
+       c[j] = c[j] + container[i][j];
+        c[j]%=1000000000;
+      } 
+
+       cout<<j<<" "<<c[j]<<endl;
+    }
     
-    
+    x=0;
+    int cnt=0;
     for(i=0;i<container.size();i++)
     {
-        a=r[i];
+        
+       /* a=r[i];
         b=c[i];
-        if(a==b)
-            return("Possible");
-        if(a!=b)
+         if(a==b)
         {
-            break;
-            return("Impossiple");
-        }   
-    }
-  
-    return("Impossible");
-}
+            x++;
+        }*/
+     for(j=0;j<container.size();j++)
+     {
+         if(r[i]==c[j])
+         {
+             cnt++;
+             break;
+         }
+     }
 
+     
+    } 
+    if(cnt==container.size())
+         return("Possible");
+         else
+   return("Impossible");
+    cout<<x<<"      okoko"<<endl;
+       if(x==container.size())
+            return("Possible");
+       else
+   return("Impossible");   
+           
+    
+}
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
