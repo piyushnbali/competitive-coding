3fbc008922cdbe49615ba0bbd35a9db47b8f1f2c
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-04-29 11:19:52 +0000

    Update and rename Handshakes.cpp to Dislikes_And_Party(Handshakes).cpp

--- Siddhesh/Hackerearth/Dislikes_And_Party(Handshakes).cpp
+++ Siddhesh/Hackerearth/Dislikes_And_Party(Handshakes).cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main(){
-  long long i,j,x,cases,hs=0;
+  /* test cases failed while I tried to use int, so I have used long long */
+  long long i,j,x,cases,hs;
+  
+  /* used a set of pairs to avoid duplication of pairs */
   set<pair<long long,long long>> s;
   long long v[10][10];
-
-  //Input
+  
+  //input
   cin>>cases;
   for (i=0;i<10;++i){
     for (j=0;j<10;++j){
@@ -16,6 +19,7 @@ int main(){
   }
   
   //solution
+  /* making all the possible pairs (with 0th index) in each row of 10x10 matrix and inserting them in set */
   for (i=0;i<10;++i){
     for (j=1;j<10;++j){
       if (v[i][0]<v[i][j]){
@@ -26,8 +30,8 @@ int main(){
       }
     }
   }
-  hs = cases*(cases-1);
-  hs = hs/2;
+  /* final answer= all possible handshakes - the size of set i.e. the number of pairs that would not shake hands */
+  hs = (cases*(cases-1))/2;
   cout<<hs-s.size();
   return 0;
 }
