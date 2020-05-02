32efd5ff7121229a04be9ef7c266a2969f67c886
siddheshkothadi <realsiddheshko@gmail.com>
2020-05-03 00:45:07 +0530

    Added hackerrank directory

---
+++ Siddhesh/Hackerrank/CavityMap.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the cavityMap function below.
+
+int main()
+{
+    int size,i,j;
+    char x;
+    cin>>size;
+    char v[size][size];
+
+    //input
+    for (i=0;i<size;++i){
+        for (j=0;j<size;++j){
+            cin>>x;
+            v[i][j]=x;
+        }
+    }
+
+    for (i=1;i<size-1;++i){
+        for (j=1;j<size-1;++j){
+            if (v[i][j]>v[i][j-1] &&
+            v[i][j]>v[i-1][j] &&
+            v[i][j]>v[i][j+1] &&
+            v[i][j]>v[i+1][j]
+            ){
+                v[i][j]='X';
+            }
+        }
+    }
+
+    for (i=0;i<size;++i){
+        for (j=0;j<size;++j){
+            cout<<v[i][j];
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
