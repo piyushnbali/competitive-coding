4ace3a0e49983c534b98991303e0d55b46c52b10
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-05-09 18:55:25 +0530

    Update and rename RemoveDuplicatesFromSortedArrayUsingUnique.cpp to RemoveDuplicatesFromSortedArray.cpp

--- Siddhesh/Leetcode/RemoveDuplicatesFromSortedArray.cpp
+++ Siddhesh/Leetcode/RemoveDuplicatesFromSortedArray.cpp
@@ -1,9 +1,39 @@
 //*******Using UNIQUE*********
+    //Using erase- 20ms
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         return nums.size();
+    }
+};
+    //Using resize-12ms
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.resize(distance(nums.begin(),unique(nums.begin(), nums.end())));
+        return nums.size();
+    }
+};
+
+//Faster than 99.7%
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size()==0){
+            return 0;
+        }
+        if (nums.size()==1){
+            return 1;
+        }
+        int index=0,i,count=1;
+        for (i=1;i<nums.size();++i){
+            if (nums[index]!=nums[i]){
+                nums[++index]=nums[i];
+                ++count;
+            }
+        }
+        return count;
     }
 };
 
