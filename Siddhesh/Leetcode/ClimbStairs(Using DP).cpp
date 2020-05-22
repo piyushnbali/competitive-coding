bd1cc284281eb47aefc19da9cb77d6aa6e510756
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-05-22 11:55:55 +0000

    Update ClimbStairs(Using DP).cpp

--- Siddhesh/Leetcode/ClimbStairs(Using DP).cpp
+++ Siddhesh/Leetcode/ClimbStairs(Using DP).cpp
@@ -1,6 +1,7 @@
 // Nothing but just Fibonacci sequence.
 // Using DP is the key here.. else time limit exceeds.
 
+//ITERATION
 class Solution {
 public:
     int climbStairs(int n) {
@@ -12,5 +13,27 @@ public:
             v[i]=v[i-1]+v[i-2];
         } 
         return v[n];
+    }
+};
+
+//RECURSION
+class Solution {
+public:
+    static int ans(vector<int>& v, int n){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(v[n]!=-1){
+            return v[n];
+        }
+        v[n]=ans(v,n-1)+ans(v,n-2);
+        return v[n];
+    }
+    int climbStairs(int n) {
+        vector<int> v(n+1,-1);
+        if(n>1){
+            v[0]=1;
+            v[1]=1;
+        }
+        return ans(v,n);
     }
 };
