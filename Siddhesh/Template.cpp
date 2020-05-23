a677d8c2bb139c24eb539cc2fd5499d1acb3a02a
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-05-23 17:02:34 +0530

    Update Template.cpp

--- Siddhesh/Template.cpp
+++ Siddhesh/Template.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define FILE freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdin);
-#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define pb push_back
 #define ll long long
 #define vi vector<int>
