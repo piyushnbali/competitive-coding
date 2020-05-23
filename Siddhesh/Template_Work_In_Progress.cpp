b863cd9c0aaedacf548ed863d2ee397c422a2368
siddheshkothadi <realsiddheshko@gmail.com>
2020-05-23 16:22:50 +0530

    added #defines

--- Siddhesh/Template_Work_In_Progress.cpp
+++ Siddhesh/Template_Work_In_Progress.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long
+#define vi vector<int>
+#define vii vector<int,int>
+#define pi pair<int,int>
+#define MP make_pair
+#define B begin()
+#define E end()
+#define pf printf
+#define sf scanf
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	FAST
     return 0;
 }
