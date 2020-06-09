faf1fea3d9cc7cf0522294ea66f9874ce5ebb298
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-06-09 18:08:55 +0530

    Update ReverseLinkedList.cpp

--- Siddhesh/Leetcode/ReverseLinkedList.cpp
+++ Siddhesh/Leetcode/ReverseLinkedList.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(!head || !head->next) return head;
-        ListNode *p=reverseList(head->next);
+        ListNode *p=reverseList(head->next); // p will actually be the last node i.e. the first node after reversing
         head->next->next=head;
         head->next=nullptr;
         return p;
