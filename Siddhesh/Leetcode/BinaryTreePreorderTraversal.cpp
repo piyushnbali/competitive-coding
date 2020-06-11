905f97b3f683bb4c934d457c4a8d70ed86e1bed7
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-06-11 16:44:59 +0530

    Create BinaryTreePreorderTraversal.cpp

---
+++ Siddhesh/Leetcode/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Iterative solution using stack
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> v;
+        v.push_back(root->val);
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()){
+            TreeNode *main=s.top();
+            if(main->left){
+                s.push(main->left);
+                v.push_back(main->left->val);
+                main->left=nullptr;
+            }
+            else if(main->right){
+                s.push(main->right);
+                v.push_back(main->right->val);
+                main->right=nullptr;
+            }
+            else{
+                s.pop();
+            }
+        }
+        return v;
+    }
+};
