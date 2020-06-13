9b856082f5a0794320e2e274b2e3458db7200fd5
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-06-13 16:03:05 +0530

    Create MaximumDepthOfBinaryTree.cpp

---
+++ Siddhesh/Leetcode/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int recur(TreeNode* root,int count){
+        if(!root) return count;
+        int left=recur(root->left,count+1);
+        int right=recur(root->right,count+1);
+        return max(left,right);
+    }
+    
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        
+        //passesd 0 instead of 1 (i.e. initial depth) because the recur returns when the root is null
+        //which is why count is always 1 greater than the actual depth
+        return recur(root,0);
+    }
+};
